8f7fc8fafa627568edba5ad343e1534baddae2d1
sokevinjonas <kjonasdevpro@gmail.com>
2025-03-04 18:06:27 +0000

    implementations buttons en flutter

--- lib/main.dart
+++ lib/main.dart
@@ -3,7 +3,7 @@ import 'package:flutter/material.dart';
 void main(){
 runApp(MaterialApp(
   theme: ThemeData(
-    colorSchemeSeed: Colors.green
+    colorSchemeSeed: Colors.blue
   ),
   home: const HomePage(),
 ));
@@ -16,16 +16,67 @@ class HomePage extends StatefulWidget{
 }
 
 class HomePageState extends State<HomePage> {
+  Color bg = Colors.white;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: bg,
       appBar: AppBar(
         title: const Text("App Bar"),
         elevation: 0,
       ),
-      body: const Center(
-        child: Text("Bienvenu"),
+      body: ListView(
+        padding: const EdgeInsets.all(40),
+        children: [
+          ElevatedButton(
+            onPressed: (){
+              setState(() {
+                bg = Colors.orange;
+              });
+            },
+            style: ElevatedButton.styleFrom(
+             backgroundColor: Colors.orange
+            ),
+            child: const Text("Button avec Elevation"),
+          ),
+          OutlinedButton(
+            style: OutlinedButton.styleFrom(
+              backgroundColor: Colors.red,
+            ),
+              onPressed: (){
+                setState(() {
+                  bg = Colors.red;
+                });
+              },
+              child: const Text("Button avec contoure")
+          ),
+          TextButton(
+            style: TextButton.styleFrom(
+              backgroundColor: Colors.green
+            ),
+            child: const Text("Button avec Texte"),
+            onPressed: (){
+              setState(() {
+                bg = Colors.green;
+              });
+            },
+          ),
+          IconButton(
+            style:  IconButton.styleFrom(
+              backgroundColor: Colors.yellow
+            ),
+              onPressed: (){
+                setState(() {
+                  bg = Colors.yellow;
+                });
+              },
+              icon: const Icon(Icons.home))
+        ],
       ),
+        floatingActionButton: FloatingActionButton(
+          onPressed: (){},
+          child: const Icon(Icons.add),
+        ),
     );
   }
 }
